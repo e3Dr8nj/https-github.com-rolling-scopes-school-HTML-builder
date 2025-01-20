bb449578e9a93f7770dabcb01136a71507d65826
e3dr8nj <hy421725@gmail.com>
2025-01-20 20:12:06 +0300

    ref: 02-write-file implement functionality with readline use

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,6 +1,8 @@
+/*
 const fs = require('fs');
 const path = require('path');
 const filePath = path.join(__dirname,"text.txt");
+
 fs.writeFile(filePath,'','utf8',(err)=>{
     if(err){ 
      console.error("error creating file",err.message);
@@ -26,7 +28,58 @@ fs.writeFile(filePath,'','utf8',(err)=>{
 process.on('SIGINT',()=>{
     exit();
 })
+  //appendFile();
 function exit(){
     process.stdout.write('Good bye\n');
     process.exit(0);
+}
+*/
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname,"text.txt");
+const readline = require('readline');
+fs.writeFile(filePath,'','utf8',(err)=>{
+    if(err){ 
+     console.error("error creating file",err.message);
+    }else{
+     console.log("file created");
+     startInput();
+    }
+   })
+function startInput(){
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  process.stdout.write('Input some text ,type "exit" to quit:');
+  rl.on('line',(input)=>{
+    if(input.trim()==="exit"){
+        rl.close();
+    }else{
+        appendFile(input);
+        
+    }
+  })
+  rl.on('close',()=>{
+    exit();
+  })
+
+process.on('SIGINT',()=>{
+    rl.close();
+})
+}
+
+function exit(){
+    process.stdout.write('Good bye\n');
+    process.exit(0);
+}
+function appendFile(input){
+    fs.appendFile(filePath, input + '\n', 'utf-8', (err) => {
+        if (err) {
+            console.error('Error appending to file:', err.message);
+        } else {
+           
+            process.stdout.write('Input some text ,type "exit" to quit:');
+        }
+    });
 }
