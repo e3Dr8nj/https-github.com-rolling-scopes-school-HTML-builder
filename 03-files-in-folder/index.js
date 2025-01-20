e399dd16978ee37f68a13038132d3d99bd24c686
e3dr8nj <hy421725@gmail.com>
2025-01-20 22:29:07 +0300

    ref: 3-files-in-folder refactor with asinc promises

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,7 +1,8 @@
+/*
 const fs = require('fs');
 const path= require('path');
 const dirPath = path.join(__dirname,'secret-folder');
-console.log(dirPath);
+/console.log(dirPath);
 fs.readdir(dirPath, {withFileTypes:true}, (err,files)=>{
     if(err){console.error("error occurs due reading files ",err.message);return};
    // console.log(files);
@@ -17,3 +18,25 @@ fs.readdir(dirPath, {withFileTypes:true}, (err,files)=>{
       }
     });
 })
+    */
+const fs = require('fs');
+const path= require('path');
+const dirPath = path.join(__dirname,'secret-folder');
+async function readDir(){
+ let files = await fs.promises.readdir(dirPath,{withFileTypes: true},(err,files)=>{
+    if(err){console.error('error due reading dir');}
+    return files;
+  })
+   files.forEach(file => {
+     const filePath = path.join(dirPath,file.name);
+     fs.stat(filePath,(err,stats)=>{
+        if(err){console.error('error due reading info about files',err.message);return;}
+       if(stats.isFile()){
+         const extName = path.extname(file.name);
+         const size = stats.size;
+         process.stdout.write(`${file.name}-${extName}-${(size/1024).toFixed(3)}kb \n`)
+       }
+     })
+   });
+}
+readDir();
