c2f21539e38348fab8112a5b36e11098b9ed382e
e3dr8nj <hy421725@gmail.com>
2025-01-20 19:06:38 +0300

    add: 02-write-file impliment creating text file and appendFile function

---
+++ 02-write-file/index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname,"text.txt");
+fs.writeFile(filePath,'','utf8',(err)=>{
+    if(err){ 
+     console.error("error creating file",err.message);
+    }else{
+     console.log("file created")
+    }
+   })
+ function appendFile(){  
+   process.stdout.write('input data \n')
+   process.stdin.on("data",(data)=>{
+  fs.appendFile(filePath,data,'utf-8',(err)=>{
+    if(err){
+      console.error('error due appen file process');
+    }else{
+      console.log('file updated')
+    }
+  })
+
+})
+return appendFile();
+}
+appendFile();
+process.on('exit',(exit)=>{
+    if(exit==0){
+      process.stdout.write('bye');
+      process.exit();
+    }else{
+      process.stderr.write(`${error}`)
+    }
+})
