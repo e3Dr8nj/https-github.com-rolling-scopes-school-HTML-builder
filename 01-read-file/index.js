af8f5d73c1a9031e0f8fc70766f87fa4f4dcdd4c
e3dr8nj <hy421725@gmail.com>
2025-01-20 18:30:46 +0300

    refactor: updata 01-read-file task by implement checking file existense and using promisses

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,3 +1,4 @@
+/*
 const fs = require('fs');
 const path = require('path');
 function readFile(){
@@ -6,5 +7,23 @@ const filePath = path.join(__dirname,"text.txt");
 const readStream = fs.createReadStream(filePath,'utf8');
 readStream.pipe(process.stdout);
 readStream.on('error',(err)=>{console.error('error occurs in process reading the file : ',err.message)})
+}
+readFile()
+*/
+const fs = require('fs');
+
+const path = require('path');
+async function readFile(){
+    const filePath = path.join(__dirname,"text.txt");
+    try{
+      await fs.promises.access(filePath);
+      const readStream = fs.createReadStream(filePath,'utf8');
+      readStream.pipe(process.stdout);
+      readStream.on('error',(err)=>{
+        console.error('an error occurs due reading file process',err.message)
+      })
+    }catch(err){
+        console.error(`file does not exist on unaccessable `,err.message);
+    }
 }
 readFile()
